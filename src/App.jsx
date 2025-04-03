88ece16247936f1a100b3d119d0122a60b63342a
Stanislav Ryazantsev <ryazancev_stas94@mail.ru>
2025-04-03 13:26:32 +0700

    homework

--- src/App.jsx
+++ src/App.jsx
@@ -4,32 +4,184 @@ import styles from './App.module.css';
 function App() {
 	const [todos, setTodos] = useState([]);
 	const [isLoading, setIsLoading] = useState(false);
+	const [modalAddCase, setModalAddCase] = useState(false);
+	const [caseTitle, setCaseTitle] = useState('');
+	const [editTitle, setEditTitle] = useState('');
+	const [modalEditCase, setModalEditCase] = useState(false);
+	const [selectedTodoId, setSelectedTodoId] = useState(null);
+	const [sortTodosOn, setSortTodosOn] = useState(false);
+	const [searchQuery, setSearchQuery] = useState('');
 
-	useEffect(() => {
+	const todosSort = () => {
+		setSortTodosOn(!sortTodosOn);
+	};
+
+	const getSortedTodos = () => {
+		if (sortTodosOn) {
+			return [...todos].sort((a, b) => a.title.localeCompare(b.title));
+		}
+		return todos;
+	};
+
+	const filteredTodos = getSortedTodos().filter((todo) =>
+		todo.title.toLowerCase().includes(searchQuery.toLowerCase()),
+	);
+
+	const handleInputChange = (event) => {
+		setCaseTitle(event.target.value);
+	};
+
+	const handleEditInputChange = (event) => {
+		setEditTitle(event.target.value);
+	};
+
+	const handleSearchInputChange = (event) => {
+		setSearchQuery(event.target.value);
+	};
+
+	const fetchTodos = () => {
 		setIsLoading(true);
-
-		fetch('https://jsonplaceholder.typicode.com/todos')
+		fetch('http://localhost:3005/todos')
 			.then((loadedData) => loadedData.json())
 			.then((loadedTodos) => setTodos(loadedTodos))
-			.catch((err) => console.error(err))
 			.finally(() => setIsLoading(false));
+	};
+
+	useEffect(() => {
+		fetchTodos();
 	}, []);
+
+	const modalPanel = () => {
+		return (
+			<div className={styles.addTodoPanel}>
+				<input
+					type="text"
+					className={styles.addTodoInput}
+					value={modalAddCase ? caseTitle : editTitle}
+					onChange={modalAddCase ? handleInputChange : handleEditInputChange}
+					placeholder={
+						modalAddCase ? 'Введите название дела' : 'Введите изменение'
+					}
+				/>
+				<button
+					className={styles.addTodoButton}
+					onClick={modalAddCase ? handleAddCase : handleEditCase}
+				>
+					Принять
+				</button>
+			</div>
+		);
+	};
+
+	const requestAddCase = () => {
+		setModalAddCase(true);
+		setModalEditCase(false);
+		setCaseTitle('');
+	};
+
+	const handleAddCase = () => {
+		fetch('http://localhost:3005/todos', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json;charset=utf-8' },
+			body: JSON.stringify({
+				title: caseTitle.charAt(0).toUpperCase() + caseTitle.slice(1),
+			}),
+		})
+			.then((rawResponse) => rawResponse.json())
+			.then(() => {
+				fetchTodos();
+				setModalAddCase(false);
+				setCaseTitle('');
+			});
+	};
+
+	const requestDeleteCase = (id) => {
+		fetch(`http://localhost:3005/todos/${id}`, {
+			method: 'DELETE',
+		})
+			.then((rawResponse) => rawResponse.json())
+			.then(() => {
+				fetchTodos();
+			});
+	};
+
+	const requestEditCase = (id, title) => {
+		setModalEditCase(true);
+		setModalAddCase(false);
+		setSelectedTodoId(id);
+		setEditTitle(title);
+	};
+
+	const handleEditCase = () => {
+		fetch(`http://localhost:3005/todos/${selectedTodoId}`, {
+			method: 'PUT',
+			headers: { 'Content-Type': 'application/json;charset=utf-8' },
+			body: JSON.stringify({
+				title: editTitle.charAt(0).toUpperCase() + editTitle.slice(1),
+			}),
+		})
+			.then((rawResponse) => rawResponse.json())
+			.then(() => {
+				fetchTodos();
+				setModalEditCase(false);
+				setSelectedTodoId(null);
+				setEditTitle('');
+			});
+	};
 
 	return (
 		<>
 			<div className={styles.appContainer}>
 				<h1>Список дел</h1>
+				<input
+					type="text"
+					placeholder="Поиск дел..."
+					value={searchQuery}
+					onChange={handleSearchInputChange}
+					style={{ width: '100%' }}
+				/>
+				<button className={styles.button} onClick={requestAddCase}>
+					Добавить
+				</button>
+
+				<button
+					className={styles.button}
+					onClick={todosSort}
+					style={{
+						backgroundColor: sortTodosOn ? '#4CAF50' : '#f44336',
+						color: 'white',
+					}}
+				>
+					{sortTodosOn ? `Sort On \u2191` : 'Sort Off \u2193'}
+				</button>
+
+				{modalAddCase ? modalPanel() : null}
+
 				{isLoading ? (
 					<div className={styles.loader}></div>
 				) : (
 					<ul className={styles.todoList}>
-						{todos.map((todo) => (
+						{filteredTodos.map((todo, index) => (
 							<li className={styles.todoItem} key={todo.id}>
-								{todo.title}
+								{index + 1}. {todo.title}
+								<button
+									className={styles.buttonCase}
+									onClick={() => requestDeleteCase(todo.id)}
+									style={{ backgroundColor: 'red' }}
+								>
+									Удалить
+								</button>
+								<button
+									className={styles.buttonCase}
+									onClick={() => requestEditCase(todo.id, todo.title)}
+								>
+									Изменить
+								</button>
 							</li>
 						))}
 					</ul>
 				)}
+				{modalEditCase ? modalPanel() : null}
 			</div>
 		</>
 	);
---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import {Database, getDatabase} from 'firebase/database'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAoUbIXDpMvlrybKWU7C-LHdXK5c2rfOMk",
+  authDomain: "case-list-1932e.firebaseapp.com",
+  projectId: "case-list-1932e",
+  storageBucket: "case-list-1932e.firebasestorage.app",
+  messagingSenderId: "699393849304",
+  appId: "1:699393849304:web:b644026acb393bf8fc24ad",
+  databaseURL: "https://case-list-1932e-default-rtdb.europe-west1.firebasedatabase.app/",
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app)
